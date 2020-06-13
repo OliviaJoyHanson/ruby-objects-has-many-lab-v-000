2508350ebf9ec7c097092ffced41173e44bcd419
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-06-13 17:16:39 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,32 +2,32 @@ require 'pry'
 
 class Artist
   attr_accessor :name
-
-  @@all = []
+  @@song_count = 0
 
   def initialize(name)
     @name = name
-    @@all << self
+    @songs = []
   end
 
-  def self.all
-    @@all
+  def songs
+    @songs
   end
 
   def add_song(song)
+    @songs << song
     song.artist = self
+    @@song_count += 1
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
+    @songs << song
     song.artist = self
-  end
-
-  def songs
-    Song.all.select {|song| song.artist == self}
+    @@song_count += 1
   end
 
   def self.song_count
-    Song.all.count
+    @@song_count
   end
+
 end
